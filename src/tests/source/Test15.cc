8a1bc0afc0e233a8b057fc1d1a43052d20611779
Christopher Jermaine <cmj4@rice.edu>
2016-08-26 01:55:31 +0000

    bunch of changes
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2177 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/tests/source/Test15.cc
@@ -0,0 +1,38 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef TEST_15_CC
+#define TEST_15_CC
+
+#include "PDBServer.h"
+#include "CatalogServer.h"
+#include "CatalogClient.h"
+
+int main () {
+
+       std :: cout << "Starting up a server!!  Make sure to run bin/test14 to perform the full test.\n";
+       pdb :: PDBLoggerPtr myLogger = make_shared <pdb :: PDBLogger> ("frontendLogFile.log");
+       pdb :: PDBServer frontEnd (8108, 10, myLogger);
+       frontEnd.addFunctionality <pdb :: CatalogServer> ("CatalogDir");
+       frontEnd.addFunctionality <pdb :: CatalogClient> (8108, "localhost", myLogger);
+
+       frontEnd.startServer (nullptr);
+}
+
+#endif
+
