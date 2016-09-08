e1b066e83e93e13cd5d4bdc0ec8698cc40bbc2c3
jia-bigdata <jacquelinezou@gmail.com>
2016-09-08 17:41:17 +0000

    please run Test22.cc and Test23.cc
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2210 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/builtInPDBObjects/headers/StorageAddSet.h
@@ -0,0 +1,65 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef STORAGE_ADD_SET_H
+#define STORAGE_ADD_SET_H
+
+#include "Object.h"
+#include "Handle.h"
+#include "PDBString.h"
+
+// PRELOAD %StorageAddSet%
+
+namespace pdb {
+
+// encapsulates a request to add a set in storage
+class StorageAddSet  : public Object {
+
+public:
+
+	StorageAddSet () {}
+	~StorageAddSet () {}
+
+	StorageAddSet (std :: string dataBase, std :: string setName, std :: string typeName) : dataBase (dataBase), setName (setName),
+		typeName (typeName) {}
+
+	std :: string getDatabase () {
+		return dataBase;
+	}
+
+	std :: string getSetName () {
+		return setName;
+	}
+
+	std :: string getType () {
+		return typeName;
+	}
+
+	ENABLE_DEEP_COPY
+
+private:
+
+	String dataBase;
+	String setName;
+	String typeName;
+
+};
+
+}
+
+#endif
