aff623f306804b8b051ec89670b7d5b91510c212
Kia Teymourian <kiat@rice.edu>
2016-09-20 18:24:54 +0000

    Minor bug fixes
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2279 522aa1da-077c-40de-a48b-1771163b7009

--- pdbClusterStartup.sh
+++ pdbClusterStartup.sh
@@ -17,7 +17,7 @@
 PDB_CLUSTER_CONFIG_FILE="pdbCluster.config"
 PEM_FILE=$1
 PDB_HOME="PDBServer/"
-PDB_COMMAND="./pdbServer"
+PDB_COMMAND="pdbServer"
 PDB_STARTUP="pdbStartup.sh"
 PDB_DIRS1="bin/pdbServer"
 PDB_DIRS2="libraries/"
