0e426a5cded178a27e5e588848cd5e1f2fc56584
Kia Teymourian <kiat@rice.edu>
2016-10-05 19:17:45 +0000

    added a cleanup script for simple nodes
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2387 522aa1da-077c-40de-a48b-1771163b7009

---
+++ tools/utils/cleanup.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#  Copyright 2018 Rice University
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ========================================================================    
+DATA_DIRS="pdbRoot catalog"
+
+echo "Removing old folders and killing PDB processes. "
+
+
+echo "Removing old files ..."
+
+# remove old files 
+if [ "$1" = "y" ]; then 
+
+rm -rf $DATA_DIRS ; 
+
+fi;
+
+echo "Killing Process pdbServer"
+
+for KILLPID in `ps ax | grep '[p]dbServer' | awk ' { print $1;}'`; do 
+  kill -9 $KILLPID;
+done
+
+echo "sleep 2 sec and kill again pdbServer processes"
+
+sleep 2
+
+
+for KILLPID in `ps ax | grep '[p]dbServer' | awk ' { print $1;}'`; do 
+  kill -9 $KILLPID;
+done
+
+echo "Done!"
+
+
+
+
