5b2b9665c7d9882e16e0932958681be34e6c1c4b
jia-bigdata <jacquelinezou@gmail.com>
2016-10-09 17:43:50 +0000

    fixed two bugs in linux
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2398 522aa1da-077c-40de-a48b-1771163b7009

--- src/builtInPDBObjects/headers/JobStage.h
+++ src/builtInPDBObjects/headers/JobStage.h
@@ -143,7 +143,7 @@ namespace pdb {
             Handle<SetIdentifier> output;
 
             //Output type name
-            std :: string outputTypeName;
+            String outputTypeName;
 
             //operator information
             Vector<Handle<ExecutionOperator>> operators;
