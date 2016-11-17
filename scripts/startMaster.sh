331ac1c0fc24bf4138551f1fcb16c2275cf07c2e
jia-bigdata <jacquelinezou@gmail.com>
2016-11-17 18:47:41 +0000

    scripts and new test case for DSM and catalog error
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2610 522aa1da-077c-40de-a48b-1771163b7009

---
+++ scripts/startMaster.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+#  Copyright 2018 Rice University
+#
+#  Licensed under the Apache License, Version 2.0 (the "License");
+#  you may not use this file except in compliance with the License.
+#  You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#  Unless required by applicable law or agreed to in writing, software
+#  distributed under the License is distributed on an "AS IS" BASIS,
+#  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#  See the License for the specific language governing permissions and
+#  limitations under the License.
+#  ========================================================================    
+# by Jia
+
+$PDB_HOME/scripts/cleanupNode.sh
+sleep 5
+$PDB_HOME/bin/test404 localhost 8108 &
+sleep 10
+$PDB_HOME/bin/CatalogServerTests 1 localhost 8108 localhost 8108 master master
