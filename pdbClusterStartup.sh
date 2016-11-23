3922c01964cef8f16cf0ea60b023afc99e1f35b6
Kia Teymourian <kiat@rice.edu>
2016-11-23 21:02:34 +0000

    changed pdb Cluster start up script
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2646 522aa1da-077c-40de-a48b-1771163b7009

--- pdbClusterStartup.sh
+++ pdbClusterStartup.sh
@@ -14,7 +14,7 @@
 #  ======================================================================== 
 #!/bin/bash
 
-PDB_CLUSTER_CONFIG_FILE="pdbCluster.config"
+PDB_CLUSTER_CONFIG_FILE="./conf/serverlist"
 PEM_FILE=$1
 PDB_HOME="PDBServer"
 PDB_COMMAND="pdbServer"
@@ -24,14 +24,16 @@ PDB_STARTUP="pdbStartup.sh"
 PDB_DIRS1="bin"
 PDB_DIRS2="libraries"
 PDB_DIRS3="conf"
+SSHPort="22"
+USER="ubuntu"
 
 
 
 ##############  Function to Copy Files to each node ####################
 readClusterConfigAndCopy() {
 
-old_IFS=$IFS  # save the field separator
-IFS=$'\n'     # new field separator, the end of line
+# old_IFS=$IFS  # save the field separator
+# IFS=$'\n'     # new field separator, the end of line
 
 echo "Creating the tar.gz file to copy to cluster nodes... "; 
 
@@ -42,19 +44,21 @@ for line in $(cat "$1")
 do
 
 # parse the line to an array
-IFS='#' read -ra ADDR <<< "$line"
+# IFS='#' read -ra ADDR <<< "$line"
 
 # first create the director - if exists remove and make it
-ssh  -i $2 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  ${ADDR[0]} "rm -rf $3 && mkdir -p $3" ;
+ssh  -i $2 -p $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  $USER@$line "rm -rf $3 && mkdir -p $3" ;
 
 # Copy tar file over
-scp  -i $2 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r $3.tar.gz  ${ADDR[0]}":" ;
+scp  -i $2 -P $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -r $3.tar.gz  $USER@$line":" ;
+
 # SSH to the machine and untar and remove file
-ssh  -i $2 -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  ${ADDR[0]} "tar xvfz  $3.tar.gz  -C $3 && rm -f $3.tar.gz " ;
+ssh  -i $2 -p $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  $USER@$line "tar xvfz  $3.tar.gz  -C $3 && rm -f $3.tar.gz " ;
 
 
 done
-IFS=$old_IFS     # restore default field separator
+# IFS=$old_IFS     # restore default field separator 
+# $line was ${ADDR[0]}
 
 } 
 
@@ -62,21 +66,40 @@ IFS=$old_IFS     # restore default field separator
 ##############  Function to Run pdbServer on each node ####################
 readClusterConfigAndRun() {
 
-old_IFS=$IFS  # save the field separator
-IFS=$'\n'     # new field separator, the end of line
+# old_IFS=$IFS  # save the field separator
+# IFS=$'\n'     # new field separator, the end of line
+
+COUNTER=0
+
 
 for line in $(cat "$1")
 do
 
 # parse the line to an array
-IFS='#' read -ra ADDR <<< "$line"
+# IFS='#' read -ra ADDR <<< "$line"
 
+COUNTER=$[COUNTER + 1] 
+
+echo $COUNTER
+
+if [ $COUNTER -eq 1 ]
+then
+  echo "Start a Master Node on $line"
+  ssh  -i $2 -p $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  $USER@$line  "cd $3 && ./pdbStartup.sh " ;
+else
+  echo "Start a Slave Node on $line"
+  ssh  -i $2 -p $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  $USER@$line  "cd $3 && ./pdbStartup.sh -s" ;
+fi
+
+
+echo $line 
 
 # Now SSH to each machine and start up the pdbServer
-ssh  -i $2  -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  ${ADDR[0]}  "cd $3 && ./pdbStartup.sh  ${ADDR[1]} " ;
+
+# ssh  -i $2 -p $SSHPort -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no  USER@$line  "cd $3 && ./pdbStartup.sh -s " ;
 
 done
-IFS=$old_IFS     # restore default field separator
+# IFS=$old_IFS     # restore default field separator
 
 }
 
@@ -88,11 +111,11 @@ IFS=$old_IFS     # restore default field separator
 ###############################################
 
 
-read -p "Do you wish to copy PDB to all cluster nodes?[y/n]" yn
+# read -p "Do you wish to copy PDB to all cluster nodes?[y/n]" yn
 
 case $yn in
-   [Yy]* ) readClusterConfigAndCopy $PDB_CLUSTER_CONFIG_FILE $1 $PDB_HOME $PDB_FOLDERS_TO_COPY;;
-   [Nn]* ) echo "Not Copying only starting cluster" ;;
+  [Yy]* ) readClusterConfigAndCopy $PDB_CLUSTER_CONFIG_FILE $1 $PDB_HOME $PDB_FOLDERS_TO_COPY;;
+  [Nn]* ) echo "Not Copying only starting cluster" ;;
        * ) echo "Please answer yes or no.";;
 esac
 
