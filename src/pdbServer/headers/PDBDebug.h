d159933f8629b0e86650c97b1905329a908769e8
jia-bigdata <jacquelinezou@gmail.com>
2017-01-13 16:15:37 +0000

    Class and macros to disable std::cout when needed
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2796 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/pdbServer/headers/PDBDebug.h
@@ -0,0 +1,43 @@
+#ifndef PDB_DEBUG_H
+#define PDB_DEBUG_H
+
+//by Jia, Jan 2017
+
+#include <ostream>
+#include <iostream>
+
+/*
+ * A class used to disable std::cout and output nothing
+ * http://stackoverflow.com/questions/1389538/cancelling-stdcout-code-lines-using-preprocessor
+ */
+
+class NullStream {
+public:
+    NullStream() {}
+    NullStream& operator<<(std::ostream & (*pf)(std::ostream&)) {
+        return *this;
+    }
+    template<typename T>
+    NullStream& operator<<(T const&) {
+        return *this;
+    }
+    template<typename R, typename P> 
+    NullStream& operator<<(R & (*pf)(P &)) {
+        return *this;
+    }
+
+};
+
+
+
+
+#ifdef PDB_DEBUG
+    #define PDB_COUT std::cout
+#else
+    #define PDB_COUT NullStream()
+#endif
+
+
+
+
+#endif
