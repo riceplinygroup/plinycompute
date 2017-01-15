748c1d49bfe1890c4cedbc6805eb453add69e8b2
jia-bigdata <jacquelinezou@gmail.com>
2017-01-15 04:51:46 +0000

    Add code for Top-K
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2806 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/builtInPDBObjects/headers/BuiltinTopKInput.h
@@ -0,0 +1,64 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+#ifndef BUILTIN_TOPK_INPUT_H
+#define BUILTIN_TOPK_INPUT_H
+
+//PRELOAD %BuiltinTopKInput%
+#include "Object.h"
+#include "InterfaceFunctions.h"
+
+namespace pdb {
+
+/* A class to wrap TopK input for PDB */
+
+class BuiltinTopKInput : public Object {
+
+public:
+
+    ENABLE_DEEP_COPY
+
+    BuiltinTopKInput() {}
+    ~BuiltinTopKInput() {}
+
+    BuiltinTopKInput(double score, Handle<Object> object) {
+        this->score = score;
+        this->object = deepCopyToCurrentAllocationBlock<Object>(object);
+    }
+
+    double getScore() {
+        return score;
+    }
+
+    Handle<Object> getObject() {
+        return object;
+    }
+
+
+private:
+
+    double score;
+    Handle<Object> object;
+
+
+};
+
+}
+
+
+
+#endif
---
+++ src/builtInPDBObjects/headers/BuiltinTopKQuery.h
@@ -0,0 +1,136 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef BUILTIN_TOPK_QUERY_H
+#define BUILTIN_TOPK_QUERY_H
+
+// PRELOAD %BuiltinTopKQuery%
+#ifndef MAX_THREADS
+   #define MAX_THREADS 8
+#endif
+
+#ifndef K
+   #define K 100
+#endif
+
+//#define ACCURATE_TOPK
+
+#include "BuiltinTopKInput.h"
+#include "BuiltinTopKResult.h"
+#include "Selection.h"
+#include "PDBMap.h"
+#include <pthread.h>
+#include <stdlib.h>
+#include <time.h>
+
+
+
+namespace pdb {
+class BuiltinTopKQuery : public Selection <BuiltinTopKResult, BuiltinTopKInput> {
+
+public:
+
+       ENABLE_DEEP_COPY
+
+       BuiltinTopKQuery () { 
+          
+       }
+
+       void initialize () {
+           counters = makeObject<Map<pthread_t, unsigned long>>(MAX_THREADS);
+           partialResults = makeObject<Map<pthread_t, Handle<BuiltinTopKResult>>>(MAX_THREADS);
+
+       }
+
+
+
+       SimpleLambda <bool> getSelection (Handle <BuiltinTopKInput> &checkMe) override {
+		return makeLambda (checkMe, [&] () {
+                    return false; //getSelection will not be applied in pipeline, so simply return false
+		});
+       }
+
+
+       SimpleLambda <bool> getProjectionSelection (Handle<BuiltinTopKResult> &checkMe) override {
+                return makeLambda (checkMe, [&] () {
+                    #ifdef ACCURATE_TOPK
+                       return false; //if we do accurate topK
+                    #else
+                        if (checkMe == nullptr) {
+                            return false;
+                        } else {
+                            return true;
+                        }
+                    #endif
+                });
+       }
+
+
+       SimpleLambda <Handle <BuiltinTopKResult>> getProjection (Handle<BuiltinTopKInput> &checkMe) override {
+                 
+		return makeLambda (checkMe, [&] {
+                        pthread_t threadId = pthread_self();
+                        std::cout << "myWorkerId=" << (unsigned long) threadId << std::endl;
+                        if (counters->count(threadId) == 0) {
+                            std::cout << "to allocate slot for thread:"<<(unsigned long)(threadId)<<std::endl;
+                            (*counters)[threadId] = 0;
+                            Handle<BuiltinTopKResult> partialResult = makeObject<BuiltinTopKResult> ();
+                            partialResult->initialize();
+                            (*partialResults)[threadId] = partialResult;
+                        }  
+                        #ifndef ACCURATE_TOPK
+                        if ((*counters)[threadId] == 10000) {
+                            (*partialResults)[threadId]->reset(); //to clear partial results for last emission
+                            (*partialResults)[threadId]->initialize();
+                            (*counters)[threadId] = 0; // to clear counter for last emission
+                        }
+                        #endif
+
+                        (*partialResults)[threadId]->updateTopK(checkMe);
+                        (*counters)[threadId] ++;
+
+                        Handle<BuiltinTopKResult> ret = nullptr;
+                        #ifdef ACCURATE_TOPK
+                        return ret;
+                        #else
+                        if ((*counters)[threadId] == 10000) {
+                            ret = makeObject<BuiltinTopKResult>();
+			    ret = ((*partialResults)[threadId]);
+                            std :: cout << "emit partialresult with "<< ret->getTopK()->size() << " elements" << std :: endl;
+                        }
+                        return ret;
+                        #endif
+		});
+	}
+
+
+private:
+
+        //counters to record the number of points processed
+        Handle<Map<pthread_t, unsigned long>> counters;
+
+
+        //current aggregated TopK values
+        //each thread has a BuiltinTopKResult instance
+        Handle<Map<pthread_t, Handle<BuiltinTopKResult>>> partialResults;
+
+
+};
+
+}
+#endif
