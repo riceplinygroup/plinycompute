e99538cecbadd87ac914ef08542d4a487ee9a4de
Carlos Monroy <cm13@rice.edu>
2017-01-23 23:13:17 +0000

    more catalog-related code cleaning - removed unused params - changed parameters names - add comments TODO: more code cleaning
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2878 522aa1da-077c-40de-a48b-1771163b7009

--- src/serverFunctionalities/headers/CatalogServer.h
+++ src/serverFunctionalities/headers/CatalogServer.h
@@ -25,6 +25,23 @@
 #include "PDBCatalog.h"
 #include "CatalogClient.h"
 
+// Class for handling the Catalog Server functionality. This can be used
+// either by the Catalog Master Server or any Worker Node in the cluster
+//
+// If this is the master catalog server, it will receive a metadata registration
+// request and perform the following operations:
+//
+//   1) update metadata in the local master catalog database
+//   2) iterate over all registered nodes in the cluster and send the metadata object
+//   3) update catalog version
+
+// if this is the a worker catalog instance, it will receive a metadata registration
+// request from the master catalog server and perform the following operations:
+//
+//   1) update metadata in the local catalog database
+//   2) update catalog version
+//   3) send ack to master catalog server
+
 namespace pdb {
 
 class CatalogServer : public ServerFunctionality {
@@ -34,112 +51,122 @@ public:
 	// destructor
 	~CatalogServer ();
 
-	// these give us the port and the address of the catalog
+    // Creates a Catalog Server
+    //        catalogDirectory: the path of the location of the catalog
+    //   isMasterCatalogServer: true if this is the Master Catalog Server
+	//                          workers nodes have this parameter set to false
+    //                masterIP: the IP address of the Master Catalog
+	//              masterPort: the port number of the Master Catalog
+	//
 	CatalogServer (std :: string catalogDirectory, bool isMasterCatalogServer,
 	               std :: string masterIP, int masterPort);
 
 	// from the ServerFunctionality interface
 	void registerHandlers (PDBServer &forMe) override;
 
-	// this uses the name of the object to find the corresponding identifier
+	// Returns the type Id of a type given its name
 	int16_t searchForObjectTypeName (string objectTypeName);
 
-	// this uses the identerifer for the object to find the corresponding type name
+	// Returns the name of a type given it's type Id
 	string searchForObjectTypeName (int16_t typeIdentifier);
 
-	// this downloads the shared library associated  with the identifier, putting it at
-	// the specified location
+    // Retrieves the content of a Shared Library given it's Type Id
+	// putting it at the specified location
 	bool getSharedLibrary (int16_t identifier, vector <char> &putResultHere,
 	                       std :: string &errMsg);
 
-    // this retrieves a shared library along its registered metadata, given its name by typeName
+    // Retrieves the content of a Shared Library along with its registered metadata,
+    // given it's typeName. Typically this method is invoked by a remote machine that
+    // has no knowledge of the typeID
     bool getSharedLibraryByTypeName (std :: string typeName,
-                                     Handle <CatalogUserTypeMetadata> &itemMetadata,
-                                     string &returnedBytes, std :: string &errMsg);
+                                     Handle <CatalogUserTypeMetadata> &typeMetadata,
+                                     string &sharedLibraryBytes, std :: string &errMsg);
 
-	// this returns the type of object in the specified set, as a type name
+	// Returns the type of an object in the specified set, as a type name
 	int16_t getObjectType (string databaseName, string setName);
 
-	// this creates a new database... returns true on success
+	// Creates a new database... returns true on success
 	bool addDatabase (string databaseName, string &errMsg);
 
-    // this deletes a database... returns true on success
+    // Deletes a database... returns true on success
     bool deleteDatabase (string databaseName, string &errMsg);
 
-	// deletes a set from the database
+	// Deletes a set from the database
 	bool deleteSet (std :: string databaseName, std :: string setName, std :: string &errMsg);
 
-	// this creates a new set in a given database... returns true on success
+	// Creates a new set in a given database... returns true on success
 	bool addSet (int16_t typeIdentifier, string databaseName, string setName, string &errMsg);
 	
-    // this adds a node to a set... returns true on success
+    // Adds information about a node to a set for a given database
     // returns true on success, false on fail
     bool addNodeToSet (std :: string nodeIP, std :: string databaseName, std :: string setName,
                        std :: string &errMsg);
 
-    // this adds a node to a DB... returns true on success
+    // Adds information about a node to a database
     // returns true on success, false on fail
     bool addNodeToDB (std :: string nodeIP, std :: string databaseName, std :: string &errMsg);
 
-    // this adds a node to a set... returns true on success
+    // Removes information about a node from a set, this is invoked when storage
+    // removes a set for a database in a node in the cluster
     // returns true on success, false on fail
     bool removeNodeFromSet (std :: string nodeIP, std :: string databaseName, std :: string setName,
                             std :: string &errMsg);
 
-    // this adds a node to a DB... returns true on success
+    // Removes information about a node from a database, this is invoked when storage
+    // removes a database in a node in the cluster
     // returns true on success, false on fail
     bool removeNodeFromDB (std :: string nodeIP, std :: string databaseName, std :: string &errMsg);
 
-    // adds a new object type... return -1 on failure, this is done on a worker node catalog
+    // Adds a new object type... return -1 on failure, this is done on a worker node catalog
 	// the typeID is given by the master catalog
-    int16_t addObjectType (int16_t typeID, vector <char> &soFile, string &errMsg);
+    int16_t addObjectType (int16_t typeID, string &soFile, string &errMsg);
 
-	// print the content of the catalog metadata that have changed since a given timestamp
+	// Print the content of the catalog metadata that have changed since a given timestamp
 	bool printCatalog (string timeStamp);
 
-	// registers metadata about a node in the cluster
+	// Adds metadata about a new node in the cluster
 	bool addNodeMetadata (Handle<CatalogNodeMetadata> &nodeMetadata, std :: string &errMsg);
 
-    // registers metadata about a database in the cluster
+    // Adds metadata about a new database in the cluster
     bool addDatabaseMetadata (Handle<CatalogDatabaseMetadata> &dbMetadata, std :: string &errMsg);
 
-    // updates metadata about a database in the cluster
-    bool updateDatabaseMetadata (Handle<CatalogDatabaseMetadata> &dbMetadata, std :: string &errMsg);
-
-    // registers metadata about a set in the cluster
+    // Adds metadata about a new set in the cluster
     bool addSetMetadata (Handle<CatalogSetMetadata> &setMetadata, std :: string &errMsg);
 
-	// returns whether or not this is the master catalog server
+    // Updates metadata changes about a database in the cluster
+    bool updateDatabaseMetadata (Handle<CatalogDatabaseMetadata> &dbMetadata, std :: string &errMsg);
+
+	// Returns true if this is the master catalog server
 	bool getIsMasterCatalogServer();
 
-	// sets the type of Catalog Server
+	// Sets if this is the Master Catalog Server (true) or not (false)
 	void setIsMasterCatalogServer(bool isMasterCatalogServerIn);
 
-	// broadcast a piece of metadata to all available nodes in a cluster
+	// Broadcasts a metadata item to all available nodes in a cluster, when an update has occurred
 	// returns a map with the results from updating each node in the cluster
 	template<class Type>
 	bool broadcastCatalogUpdate (Handle<Type> metadataToSend,
 	                             map <string, pair<bool, string>> &broadcastResults,
 	                             string &errMsg);
 
-    // broadcast a piece of metadata to all available nodes in a cluster
-    // returns a map with the results from deleting that item on the
-	// catalog copy on each node in the cluster
+    // Broadcasts a metadata item to all available nodes in a cluster, when an delete has occurred
+    // returns a map with the results from updating each node in the cluster
     template<class Type>
     bool broadcastCatalogDelete (Handle<Type> metadataToSend,
                                  map <string, pair<bool, string>> &broadcastResults,
                                  string &errMsg);
 
-    // returns true if the node is already registered in the Catalog
+    // Returns true if a node is already registered in the Catalog
     bool isNodeRegistered(string nodeIP);
 
-    // returns true if the database is already registered in the Catalog
+    // Returns true if a database is already registered in the Catalog
     bool isDatabaseRegistered(string dbName);
 
-    // returns true if the set for this database is already registered in the Catalog
+    // Returns true if a set for a given database is already registered in the Catalog
     bool isSetRegistered(string dbName, string setName);
 
-	// returns a reference to the catalog
+	// Returns a reference to the underlying class that manages catalog metadata
+    // the metadata is stored in a SQLite database
 	PDBCatalogPtr getCatalog();
 
 private:
@@ -150,14 +177,10 @@ private:
     Handle<Vector <CatalogDatabaseMetadata> > _allDatabases;
     Handle<Vector <CatalogUserTypeMetadata> > _udfsValues;
 
-    // **end
-
 	// map from type name string to int, and vice/versa
 	map <string, int16_t> allTypeNames;
 	map <int16_t, string> allTypeCodes;
 
-//	// map from database name to list of sets
-//	map <string, vector <string>> allDatabases;
 		
 	// vector of nodes in the cluster
 	vector <string> allNodesInCluster;
@@ -176,22 +199,6 @@ private:
 
 	// serialize access
 	pthread_mutex_t workingMutex;
-
-    // whether or not this is the master catalog server
-    //
-    // if this is the master catalog server, it will receive a metadata registration
-    // request and perform the following operations:
-    //
-    //   1) update metadata in the local master catalog database
-    //   2) iterate over all registered nodes in the cluster and send the metadata object
-    //   3) update catalog version
-
-    // if this is the a worker catalog instance, it will receive a metadata registration
-    // request from the master catalog server and perform the following operations:
-    //
-    //   1) update metadata in the local catalog database
-    //   2) update catalog version
-    //   3) send ack to master catalog server
 
     bool isMasterCatalogServer;
 
