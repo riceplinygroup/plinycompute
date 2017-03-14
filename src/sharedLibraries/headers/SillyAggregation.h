c67fc91f56adef157045f57cd27f78105162b8d0
jia-bigdata <jacquelinezou@gmail.com>
2017-03-14 04:23:09 +0000

    Add shared libraries for new test case
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@2981 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/sharedLibraries/headers/SillyAggregation.h
@@ -0,0 +1,54 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+#ifndef SILLY_AGG_H
+#define SILLY_AGG_H
+
+//by Jia, Mar 2017
+
+#include "ClusterAggregateComp.h"
+
+using namespace pdb;
+
+class SillyAggregation : public ClusterAggregateComp <ZA_DepartmentTotal, Employee, String, double> {
+
+public:
+
+        ENABLE_DEEP_COPY
+
+        SillyAggregation() {}
+
+        SillyAggregation (int numPartitions, int batchSize) : super (numPartitions, batchSize) {}
+
+        SillyAggregation (int numPartitions, int batchSize, std :: string dbName, std :: string setName) 
+            : super (numPartitions, batchSize, dbName, setName) {}
+
+
+        // the key type must have == and size_t hash () defined
+        Lambda <String> getKeyProjection (Handle <Employee> &aggMe) override {
+                return makeLambdaFromMember (aggMe, department);
+        }
+
+        // the value type must have + defined
+        Lambda <double> getValueProjection (Handle <Employee> &aggMe) override {
+                return makeLambdaFromMethod (aggMe, getSalary);
+        }
+
+};
+
+
+#endif
