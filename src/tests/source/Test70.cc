dbb52a62e44de4fb09b383c34758665eb68a1e80
jia-bigdata <jacquelinezou@gmail.com>
2017-04-12 01:55:15 +0000

    More test cases
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3040 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/tests/source/Test70.cc
@@ -0,0 +1,70 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef TEST_70_H
+#define TEST_70_H
+
+
+//by Jia, Mar 2017
+//To dump a set
+
+
+#include "PDBDebug.h"
+#include "PDBString.h"
+#include "Query.h"
+#include "Lambda.h"
+#include "QueryClient.h"
+#include "DistributedStorageManagerClient.h"
+#include "DataTypes.h"
+#include <ctime>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <chrono>
+#include <fcntl.h>
+
+/* This test uses data and selection of builtInType to demonstrate a distributed query with distributed storage */
+
+/*  Note that data size must be larger than #numTotalThreadsInCluster*#PageSize */
+/*  Below test case is tested using 8GB data in 4-node cluster, each node run 12 threads */
+using namespace pdb;
+int main (int argc, char * argv[]) {
+
+       if (argc < 5) {
+           std :: cout << "Usage: #databaseName #setName #outputFilePath #masterIp" << std :: endl;  
+       }      
+       std :: string databaseName = argv[1];
+       std :: string setName = argv[2];
+       std :: string outputFilePath = argv[3];
+       std :: string masterIp = argv[4];
+
+       pdb :: PDBLoggerPtr clientLogger = make_shared<pdb :: PDBLogger>("clientLog");
+       pdb :: DistributedStorageManagerClient temp (8108, masterIp, clientLogger);
+
+       std :: string errMsg;
+       bool ret = temp.exportSet (databaseName, setName, outputFilePath, "csv", errMsg);
+       if (ret == false) {
+           std :: cout << errMsg << std :: endl;
+       } else {
+           std :: cout << "Set successfully exported to " << outputFilePath << std :: endl;
+       }
+
+       system ("scripts/cleanupSoFiles.sh");
+}
+
+#endif
