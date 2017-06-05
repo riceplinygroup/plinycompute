03dcb6966bcdc7b98d6ac9a5a83d56aa9eec9a3e
Kia Teymourian <kiat@rice.edu>
2017-06-05 21:34:58 +0000

    tpch bench
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3139 522aa1da-077c-40de-a48b-1771163b7009

---
+++ src/tpchBench/source/tpchDataGenerator.cc
@@ -0,0 +1,407 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+#ifndef TPCH_DATA_GENERATOR_CC
+#define TPCH_DATA_GENERATOR_CC
+
+#include "CatalogClient.h"
+
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <fstream>
+#include <map>
+#include <chrono>
+#include <sstream>
+#include <vector>
+#include <ctime>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <chrono>
+#include <fcntl.h>
+
+#include "PDBDebug.h"
+#include "PDBString.h"
+#include "Query.h"
+#include "Lambda.h"
+#include "QueryClient.h"
+#include "DistributedStorageManagerClient.h"
+#include "DispatcherClient.h"
+#include "DataTypes.h"
+#include "InterfaceFunctions.h"
+
+
+#include "Part.h"
+#include "Supplier.h"
+#include "LineItem.h"
+#include "Order.h"
+#include "Customer.h"
+
+// #include "Set.h"
+//TODO: why shoud I include WriteStringSet when I want to use DispatcherClient?
+#include "WriteStringSet.h"
+
+using namespace std;
+
+// Run a Cluster on Localhost
+// ./bin/pdb-cluster localhost 8108 Y
+// ./bin/pdb-server 1 512 localhost:8108 localhost:8109
+// ./bin/pdb-server 1 512 localhost:8108 localhost:8110
+
+#define KB 1024
+#define MB (1024*KB)
+
+int main() {
+
+	// Connection info
+	string masterIp = "localhost";
+	int masterPort = 8108;
+
+	// register the shared employee class
+	pdb::PDBLoggerPtr clientLogger = make_shared<pdb::PDBLogger>("clientLog");
+
+	pdb::DistributedStorageManagerClient distributedStorageManagerClient(masterPort, masterIp, clientLogger);
+
+	pdb::CatalogClient catalogClient(masterPort, masterIp, clientLogger);
+
+	pdb::DispatcherClient dispatcherClient = DispatcherClient(masterPort, masterIp, clientLogger);
+
+	string errMsg;
+	if (!catalogClient.registerType("libraries/libPart.so", errMsg))
+		cout << "Not able to register type.\n";
+
+	if (!catalogClient.registerType("libraries/libSupplier.so", errMsg))
+		cout << "Not able to register type.\n";
+
+	if (!catalogClient.registerType("libraries/libLineItem.so", errMsg))
+		cout << "Not able to register type.\n";
+
+	if (!catalogClient.registerType("libraries/libOrder.so", errMsg))
+		cout << "Not able to register type.\n";
+
+	if (!catalogClient.registerType("libraries/libCustomer.so", errMsg))
+		cout << "Not able to register type.\n";
+
+	// now, create a new database
+	if (!distributedStorageManagerClient.createDatabase("TPCH_db", errMsg)) {
+		cout << "Not able to create database: " + errMsg;
+		exit(-1);
+	} else {
+		cout << "Created database.\n";
+	}
+
+	// now, create the sets for storing Customer Data
+	if (!distributedStorageManagerClient.createSet<Customer>("TPCH_db", "tpch_bench_set1", errMsg)) {
+		cout << "Not able to create set: " + errMsg;
+		exit(-1);
+	} else {
+		cout << "Created set.\n";
+	}
+
+	// Phase - 2:  READ and Store the data
+
+	//Specifications:
+	string scaleFactor = "0.2";
+
+	// All files to parse:
+	string PartFile = "tables_scale_" + scaleFactor + "/part.tbl";
+	string SupplierFile = "tables_scale_" + scaleFactor + "/supplier.tbl";
+	string OrderFile = "tables_scale_" + scaleFactor + "/orders.tbl";
+	string LineitemFile = "tables_scale_" + scaleFactor + "/lineitem.tbl";
+	string CustomerFile = "tables_scale_" + scaleFactor + "/customer.tbl";
+
+	// Common constructs:
+	string line;
+	string delimiter = "|";
+	ifstream infile;
+
+	// ####################################
+	// ####################################
+	// ##########                 #########
+	// ##########      Part       #########
+	// ##########                 #########
+	// ####################################
+	// ####################################
+
+	// Open "PartFile": Iteratively (Read line, Parse line, Create Objects):
+
+	infile.open(PartFile.c_str());
+
+	vector<pdb::Handle<Part>> partList;
+
+	// load up the allocator with RAM
+
+	// This does not work with 100 KB memory
+	// But it works with 1 MB or more memory.
+	pdb::makeObjectAllocatorBlock((size_t) 9000 * MB, true);
+
+//	pdb::Handle <pdb::Vector <pdb::Handle <Part>>> partList =  pdb::makeObject<pdb::Vector<pdb::Handle<Part>>> ();
+
+	map<int, pdb::Handle<Part>> partMap;
+
+	while (getline(infile, line)) {
+		stringstream lineStream(line);
+		std::vector<string> tokens;
+		string token;
+
+		while (getline(lineStream, token, '|')) {
+			tokens.push_back(token);
+		}
+
+		int partID = atoi(tokens.at(0).c_str());
+
+		if (partID % 10000 == 0)
+			cout << "Part ID: " << partID << endl;
+
+		pdb::Handle<Part> part = pdb::makeObject<Part>(atoi(tokens.at(0).c_str()), tokens.at(1), tokens.at(2), tokens.at(3), tokens.at(4), atoi(tokens.at(5).c_str()), tokens.at(6), atof(tokens.at(7).c_str()),
+				tokens.at(8));
+
+		partList.push_back(part);
+
+		//Populate the hash:
+		partMap[atoi(tokens.at(0).c_str())] = part;
+	}
+
+	infile.close();
+	infile.clear();
+
+	// ####################################
+	// ####################################
+	// ##########                 #########
+	// ##########     Supplier    #########
+	// ##########                 #########
+	// ####################################
+	// ####################################
+
+	//Open "SupplierFile": Iteratively (Read line, Parse line, Create Objects):
+	infile.open(SupplierFile.c_str());
+	vector<pdb::Handle<Supplier>> supplierList;
+	map<int, pdb::Handle<Supplier>> supplierMap;
+
+	while (getline(infile, line)) {
+		stringstream lineStream(line);
+		std::vector<string> tokens;
+		string token;
+
+		while (getline(lineStream, token, '|')) {
+			tokens.push_back(token);
+		}
+
+		int supplierKey = atoi(tokens.at(0).c_str());
+
+		pdb::Handle<Supplier> tSupplier = pdb::makeObject<Supplier>(supplierKey, tokens.at(1), tokens.at(2), atoi(tokens.at(3).c_str()), tokens.at(4), atof(tokens.at(5).c_str()), tokens.at(6));
+		supplierList.push_back(tSupplier);
+
+		if (supplierKey % 1000 == 0)
+			cout << "Supplier ID: " << supplierKey << endl;
+
+		//Populate the hash:
+		supplierMap[atoi(tokens.at(0).c_str())] = tSupplier;
+	}
+
+	infile.close();
+	infile.clear();
+
+	// ####################################
+	// ####################################
+	// ##########                 #########
+	// ##########     LineItem    #########
+	// ##########                 #########
+	// ####################################
+	// ####################################
+
+	//Open "LineitemFile": Iteratively (Read line, Parse line, Create Objects):
+	infile.open(LineitemFile.c_str());
+
+	pdb::Handle<pdb::Vector<pdb::Handle<LineItem>>>lineItemList = pdb::makeObject<pdb::Vector<pdb::Handle<LineItem>>>();
+
+	map<int, pdb::Handle<pdb::Vector<pdb::Handle<LineItem>>> >lineItemMap;
+
+	while (getline(infile, line)) {
+		stringstream lineStream(line);
+		std::vector<string> tokens;
+		string token;
+
+		while (getline(lineStream, token, '|')) {
+			tokens.push_back(token);
+		}
+
+		int orderKey = atoi(tokens.at(0).c_str());
+		int partKey = atoi(tokens.at(1).c_str());
+		int supplierKey = atoi(tokens.at(2).c_str());
+
+		pdb::Handle<Part> tPart;
+		pdb::Handle<Supplier> tSupplier;
+
+		//Find the appropriate "Part"
+		if (partMap.find(partKey) != partMap.end()) {
+			tPart = partMap[partKey];
+		} else {
+			throw invalid_argument("There is no such Part.");
+		}
+
+		//Find the appropriate "Part"
+		if (supplierMap.find(supplierKey) != supplierMap.end()) {
+			tSupplier = supplierMap[supplierKey];
+		} else {
+			throw invalid_argument("There is no such Supplier.");
+		}
+
+		pdb::Handle<LineItem> tLineItem = pdb::makeObject<LineItem>(tokens.at(0), orderKey, tSupplier, tPart, atoi(tokens.at(3).c_str()), atof(tokens.at(4).c_str()), atof(tokens.at(5).c_str()),
+				atof(tokens.at(6).c_str()), atof(tokens.at(7).c_str()), tokens.at(8), tokens.at(9), tokens.at(10), tokens.at(11), tokens.at(12), tokens.at(13), tokens.at(14), tokens.at(15));
+
+		if (orderKey % 100000 == 0)
+			cout << "LineItem ID: " << orderKey << endl;
+
+		//Populate the hash:
+		if (lineItemMap.find(orderKey) != lineItemMap.end()) {
+			// the key already exists in the map
+			lineItemMap[orderKey]->push_back(tLineItem);
+		} else {
+			// make a new vector
+			pdb::Handle<pdb::Vector<pdb::Handle<LineItem>>>lineItemList = pdb::makeObject<pdb::Vector<pdb::Handle<LineItem>>>();
+			// push in the vector
+			lineItemList->push_back(tLineItem);
+			// put in the map
+			lineItemMap[orderKey] = lineItemList;
+		}
+
+	}
+
+	infile.close();
+	infile.clear();
+
+	// ####################################
+	// ####################################
+	// ########## 				  #########
+	// ##########       Order     #########
+	// ##########                 #########
+	// ####################################
+	// ####################################
+
+	//Open "OrderFile": Iteratively (Read line, Parse line, Create Objects):
+	infile.open(OrderFile.c_str());
+
+	map<int, pdb::Handle<pdb::Vector<pdb::Handle<Order>>> >orderMap;
+
+	while (getline(infile, line)) {
+		stringstream lineStream(line);
+		std::vector<string> tokens;
+		string token;
+
+		while (getline(lineStream, token, '|')) {
+			tokens.push_back(token);
+		}
+
+		int orderKey = atoi(tokens.at(0).c_str());
+		int customerKey = atoi(tokens.at(1).c_str());
+
+		//Sanity Check:
+		if (lineItemMap.find(orderKey) == lineItemMap.end()) {
+			throw invalid_argument("There is no such Order.");
+		}
+
+		pdb::Handle<Order> tOrder = pdb::makeObject<Order>(lineItemMap[orderKey], orderKey, customerKey, tokens.at(2), atof(tokens.at(3).c_str()), tokens.at(4), tokens.at(5), tokens.at(6), atoi(tokens.at(7).c_str()),
+				tokens.at(8));
+
+		if (orderKey % 100000 == 0)
+			cout << "Order ID: " << orderKey << endl;
+
+		//Populate the hash:
+		if (orderMap.find(customerKey) != orderMap.end()) {
+			orderMap[customerKey]->push_back(tOrder);
+		} else {
+			pdb::Handle<pdb::Vector<pdb::Handle<Order>>>orderList = pdb::makeObject<pdb::Vector<pdb::Handle<Order>>>();
+			orderList -> push_back(tOrder);
+			orderMap[customerKey] = orderList;
+		}
+
+	}
+
+	infile.close();
+	infile.clear();
+
+	// ####################################
+	// ####################################
+	// ########## #########
+	// ########## Customers #########
+	// ########## #########
+	// ####################################
+	// ####################################
+
+	//Open "CustomerFile": Iteratively (Read line, Parse line, Create Objects):
+	infile.open(CustomerFile.c_str());
+
+//	vector<pdb::Handle<Customer>> customerList;
+
+	pdb::makeObjectAllocatorBlock((size_t) 15000 * MB, true);
+
+	pdb::Handle<pdb::Vector<pdb::Handle<Customer>>>  customerList = pdb::makeObject<pdb::Vector<pdb::Handle<Customer>>>();
+
+	while (getline(infile, line)) {
+		stringstream lineStream(line);
+		vector<string> tokens;
+		string token;
+
+		while (getline(lineStream, token, '|')) {
+			tokens.push_back(token);
+		}
+
+		int customerKey = atoi(tokens.at(0).c_str());
+
+		//Sanity: Deal with Customers without orders.
+		if (orderMap.find(customerKey) == orderMap.end()) {
+			pdb::Handle<pdb::Vector<pdb::Handle<Order>>>tOrderArray;
+			orderMap[customerKey] = tOrderArray;
+		}
+
+		pdb::Handle<Customer> tCustomer = pdb::makeObject<Customer>(orderMap[customerKey], customerKey, tokens.at(1), tokens.at(2), atoi(tokens.at(3).c_str()), tokens.at(4), atof(tokens.at(5).c_str()), tokens.at(6),
+				tokens.at(7));
+
+		customerList->push_back(tCustomer);
+
+		if (customerKey % 1000 == 0) {
+			cout << "Customer Key: " << customerKey << endl;
+
+		}
+
+		infile.close();
+		infile.clear();
+
+
+
+         pdb::Record <Vector <Handle <Customer>>> *myBytes = getRecord <Vector <Handle <Customer>>> (customerList);
+	     size_t sizeOfCustomers=   myBytes->numBytes();
+	     cout<< "Size of Customer Vector is: "<<sizeOfCustomers<<endl;
+
+		// store copies of the same dataset.
+		for (int i = 1; i <= 4; ++i) {
+			cout << "Storing Vector of Customers - Copy Number : " << i << endl;
+			if (!dispatcherClient.sendData<Customer>(std::pair<std::string, std::string>("tpch_bench_set1", "TPCH_db"), customerList, errMsg)) {
+				std::cout << "Failed to send data to dispatcher server" << std::endl;
+				return -1;
+			}
+		}
+
+
+
+	}
+}
+
+#endif
+
