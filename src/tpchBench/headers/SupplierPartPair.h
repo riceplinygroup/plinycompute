22a634e7b633c0a668fd2e81a4a58a8cc17db8e2
Kia Teymourian <kiat@rice.edu>
2017-06-08 22:30:44 +0000

    minor change
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3182 522aa1da-077c-40de-a48b-1771163b7009

--- src/tpchBench/headers/SupplierPartPair.h
+++ src/tpchBench/headers/SupplierPartPair.h
@@ -15,8 +15,8 @@
  *  limitations under the License.                                           *
  *                                                                           *
  *****************************************************************************/
-#ifndef SUPPLIER_DATA_H
-#define SUPPLIER_DATA_H
+#ifndef SUPPLIER_PART_PAIR_H
+#define SUPPLIER_PART_PAIR_H
 
 #include "Object.h"
 #include "PDBVector.h"
@@ -26,70 +26,43 @@
 // This class represents the aggregate final results a tuple of (customerName, map(supplierName, Vector(PartID)))
 namespace pdb {
 
-class SupplierData: public pdb::Object {
+class SupplierPartPair: public pdb::Object {
 
 public:
-	pdb::Handle<pdb::String> customerName;
+	int partKey;
+	pdb::Handle<pdb::String> suppliertName;
 
-	// for each customer the supplier sold to, the list of all partIDs sold
-	pdb::Handle<pdb::Map<pdb::String, pdb::Vector<int>>>soldPartIDs;
 
 	ENABLE_DEEP_COPY
 
 	// Default constructor:
-	SupplierData() {}
+	SupplierPartPair() {}
 
 	// Default destructor:
-	~SupplierData() {}
+	~SupplierPartPair() {}
 
 	// Constructor with arguments
-	SupplierData(pdb::Handle<pdb::String> customerName, pdb::Handle<pdb::Map<pdb::String, pdb::Vector<int>>> soldPartIDs) {
-		this->customerName= customerName;
-		this->soldPartIDs= soldPartIDs;
+	SupplierPartPair(pdb::Handle<pdb::String> supplierName, int partKey) {
+
 	}
 
+	int getPartKey() const {
+		return partKey;
+	}
+
+	void setPartKey(int partKey) {
+		this->partKey = partKey;
+	}
+
+	const pdb::Handle<pdb::String>& getSuppliertName() const {
+		return suppliertName;
+	}
+
+	void setSuppliertName(const pdb::Handle<pdb::String>& suppliertName) {
+		this->suppliertName = suppliertName;
+	}
 };
 
-// We need to overload the plus operator to merge two maps of Map<String, Vector<int>>
-inline Handle<Map<String, Vector<int>>>   &operator+ (Handle<Map<String, Vector<int>>> &lhs, Handle<Map<String, Vector<int>>> &rhs) {
-	auto iter = rhs->begin();
-	while (iter != rhs->end()) {
-		String myKey = (*iter).key;
-		if (lhs->count(myKey) == 0) {
-			try {
-				(*lhs)[myKey] = (*iter).value;
-			} catch ( NotEnoughSpace &n ) {
-				lhs->setUnused (myKey);
-				throw n;
-			}
-		} else {
 
-			size_t mySize = (*lhs)[myKey].size();
-			size_t otherSize = (*iter).value.size();
-			for (size_t i = mySize; i < mySize + otherSize; i++) {
-				try {
-
-					(*lhs)[myKey].push_back((*iter).value[i-mySize]);
-
-				} catch (NotEnoughSpace &n) {
-
-					size_t curSize = (*lhs)[myKey].size();
-					for (size_t j = mySize; j < curSize; j++) {
-						(*lhs)[myKey].pop_back();
-					}
-					for (size_t j = 0; j < (*lhs)[myKey].size(); j++) {
-						std :: cout << j << ": " << (*lhs)[myKey][j]<< ";";
-					}
-					throw n;
-
-				}
-
-			}
-		}
-		++iter;
-	}
-	return lhs;
-
-}
 }
 #endif
