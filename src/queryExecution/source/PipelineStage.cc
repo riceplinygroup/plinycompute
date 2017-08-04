d5842332c9ff58781370504c1fb6e785514eca53
jia-bigdata <jacquelinezou@gmail.com>
2017-08-04 14:35:15 +0000

    get rid of random shuffling
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3428 522aa1da-077c-40de-a48b-1771163b7009

--- src/queryExecution/source/PipelineStage.cc
+++ src/queryExecution/source/PipelineStage.cc
@@ -103,7 +103,6 @@ bool PipelineStage :: broadcastData (HermesExecutionServer * server, void * data
     }
     int numNodes = this->jobStage->getNumNodes();
     
-    std :: random_shuffle (nodeIds.begin(), nodeIds.end());
     UseTemporaryAllocationBlock tempBlock {4*1024*1024};
     for (int i = 0; i < numNodes; i++) {
                   PDBCommunicator temp;
