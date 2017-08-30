e71006a0d6a856750f566af99d472cc1d3a446cd
jia-bigdata <jacquelinezou@gmail.com>
2017-08-30 00:02:29 +0000

    Optimize KMeans: get rid of getDouble() setDouble(). For 10^6 points with 1000 dims, each iteration latency is reduced from 50 seconds to 15 seconds.
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3514 522aa1da-077c-40de-a48b-1771163b7009

--- src/builtInPDBObjects/headers/DoubleVector.h
+++ src/builtInPDBObjects/headers/DoubleVector.h
@@ -23,21 +23,26 @@
 #include "Object.h"
 #include "Handle.h"
 #include "PDBVector.h"
-
+#include "Configuration.h"
+#include <math.h>
 //PRELOAD %DoubleVector%
 
+
+#ifndef KMEANS_EPSILON
+   #define KMEANS_EPSILON 0.01
+#endif
 
 namespace pdb {
 
 class DoubleVector: public Object {
 
 public:
-     Handle<Vector<double>> data;
-     size_t size;
-
+     Handle<Vector<double>> data = nullptr;
+     size_t size = 0;
+     double norm = -1;
 public:
 
-     DoubleVector () {}
+     DoubleVector () { size = 0; }
 
      DoubleVector ( size_t size ) {
          this->size = size;
@@ -48,10 +53,10 @@ public:
      }
 
      void setValues ( std :: vector <double> dataToMe) {
+         double *  rawData = data->c_ptr();
          if (dataToMe.size() >= size) {
              for (int i = 0; i < size; i++) {
-                (*data)[i] = dataToMe[i];
-                
+                rawData[i] = dataToMe[i];
              }
          } else {
              std :: cout << "my size is " << size << ", and input's size is " << dataToMe.size()<<std :: endl;
@@ -65,6 +70,13 @@ public:
 
      Handle<Vector<double>> & getData () {
          return data;
+     }
+
+     double * getRawData () {
+         if (data == nullptr) {
+             return nullptr;
+         }
+         return data->c_ptr();
      }
 
      double getDouble (int i) {
@@ -83,54 +95,131 @@ public:
          }
      }
 
+     //following implementation of Spark MLLib
+     //https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/linalg/Vectors.scala
+     double getNorm2() {
+         double * rawData = data->c_ptr();
+         if (norm < 0 ) {
+             size_t mySize = this->getSize();
+             for ( int i = 0; i < mySize; i++) {
+                 norm += rawData[i] * rawData[i];
+             }
+             norm = sqrt(norm);
+         }
+         return norm;
+     }
+     
+     double dot (DoubleVector &other) {
+          size_t mySize = this->getSize();
+          size_t otherSize = other.getSize();
+          double * rawData = data->c_ptr();
+          double * otherRawData = other.getRawData();
+          if (mySize != otherSize) {
+              std :: cout << "Error in DoubleVector: dot size doesn't match" << std :: endl;
+              exit(-1);
+          }
+          else {
+               double dotSum = 0;
+               for (int i = 0; i < mySize; i++) {
+                   dotSum += rawData[i] * otherRawData[i];
+               }
+               return dotSum;
+          }
+     }
+
+     //to get squared distance following SparkMLLib
+    
+     double getSquaredDistance (DoubleVector &other) {
+          size_t mySize = this->getSize();
+          size_t otherSize = other.getSize();
+          double * rawData = data->c_ptr();
+          double * otherRawData = other.getRawData();
+          if (mySize != otherSize) {
+              std :: cout << "Error in DoubleVector: dot size doesn't match" << std :: endl;
+              exit(-1);
+          }
+          double distance = 0;
+          size_t kv = 0;
+          while (kv < mySize) {
+               double score = rawData[kv] - otherRawData[kv];
+               distance += score * score;
+               kv ++;
+          }
+          return distance;
+
+     }
+
 
      void print() {
+         double * rawData = data->c_ptr();
          for (int i = 0; i < this->getSize(); i++) {
-             std :: cout << i << ": " << (*data)[i] << "; ";
+             std :: cout << i << ": " << rawData[i] << "; ";
          }
          std :: cout << std :: endl;
      }
 
-     /* 
-     void push_back (Handle<double> val) {
-    	 data->push_back(*val);
+
+
+     //this implementation is following Spark MLLib
+     //https://github.com/apache/spark/blob/master/mllib/src/main/scala/org/apache/spark/mllib/util/MLUtils.scala
+     double getFastSquaredDistance (DoubleVector &other) {
+         double precision = 0.000001;
+         double myNorm = norm;
+         double otherNorm = other.getNorm2();
+         if (norm < 0) {
+             myNorm = getNorm2();
+         }
+         double sumSquaredNorm = myNorm * myNorm + otherNorm * otherNorm;
+         double normDiff = myNorm - otherNorm;
+         double sqDist = 0.0;
+         double precisionBound1 = 2.0 * KMEANS_EPSILON * sumSquaredNorm / (normDiff * normDiff + KMEANS_EPSILON);
+         if (precisionBound1 < precision) {
+             sqDist = sumSquaredNorm - 2.0 * dot (other);
+         } else {
+             sqDist = getSquaredDistance(other);
+         }
+         return sqDist;
      }
-     */
-     
+
 
      DoubleVector& operator + (DoubleVector &other) {
          //std :: cout << "me:" << this->getSize() << std :: endl;
          //this->print();
          //std :: cout << "other:" << other.getSize() << std :: endl;
          //other.print();
-
-         if (this->getSize() <= other.getSize()) {
-             for (int i = 0; i < this->getSize(); i++) {
-                 (*data)[i] += other.getDouble(i);
-             }
-                        
-         } else {
-             for (int i = 0; i < other.getSize(); i++) {
-                 (*data)[i] += other.getDouble(i);
-             }
-
+         size_t mySize = this->getSize();
+         size_t otherSize = other.getSize();
+         if (mySize != otherSize) {
+              std :: cout << "Error in DoubleVector: dot size doesn't match" << std :: endl;
+              exit(-1);
          } 
+         double * rawData = data->c_ptr();
+         double * otherRawData = other.getRawData();
+         for (int i = 0; i < mySize; i++) {
+              rawData[i] += otherRawData[i];
+         }
          return *this;
          
      }
 
      
      DoubleVector& operator / (int val) {
-	 
-
-	 Handle<DoubleVector> result = makeObject<DoubleVector>(this->getSize());
-     	 for (int i = 0; i < this->getSize(); i++) {
-		result->setDouble(i, (*data)[i] / val);
+         if (val == 0) {
+             std :: cout << "Error in DoubleVector: division by zero" << std :: endl;
+             exit(-1);
+         }	 
+         size_t mySize = this->getSize();
+         Handle<DoubleVector> result = makeObject<DoubleVector>(mySize);
+         double * rawData = data->c_ptr();
+         double * otherRawData = result->getRawData();
+     	 for (int i = 0; i < mySize; i++) {
+		otherRawData[i] = rawData[i] / val;
      	 }
 
          return *result;
          
      }
+
 
 
 
