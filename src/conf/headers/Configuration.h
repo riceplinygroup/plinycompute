f420322c7e6c4fcf87ba442f7b5c5d396f1d598c
jia-bigdata <jacquelinezou@gmail.com>
2017-10-08 17:39:27 +0000

    by default using 256MB page size for shuffling.
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3625 522aa1da-077c-40de-a48b-1771163b7009

--- src/conf/headers/Configuration.h
+++ src/conf/headers/Configuration.h
@@ -51,7 +51,7 @@ using namespace std;
 #endif
 
 #ifndef DEFAULT_SHUFFLE_PAGE_SIZE
-#define DEFAULT_SHUFFLE_PAGE_SIZE ((size_t)(64)*(size_t)(1024)*(size_t)(1024))
+#define DEFAULT_SHUFFLE_PAGE_SIZE ((size_t)(256)*(size_t)(1024)*(size_t)(1024))
 #endif
 
 #ifndef DEFAULT_BROADCAST_PAGE_SIZE
