07f19898fab272b911ac0d474cf3cf7bc7e583b4
Carlos Monroy <cm13@rice.edu>
2017-10-19 20:41:11 +0000

    methods wrappers added
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3685 522aa1da-077c-40de-a48b-1771163b7009

--- src/mainClient/headers/PDBClient.h
+++ src/mainClient/headers/PDBClient.h
@@ -86,18 +86,26 @@ namespace pdb {
         /* Creates a database */
         bool createDatabase(const std::string& databaseName, std::string& errMsg);
 
+        /* Creates a set with a given type for an existing database, no page_size needed in arg. */
+        bool createSet(const std::string& databaseName, const std::string& setName, const std::string& typeName,
+                       std::string& errMsg);
+
+        /* Creates a set with a given type (using a template) for an existing database, no page_size needed in arg. */
+        template <class DataType>
+        bool createSet (const std :: string& databaseName, const std :: string& setName, std :: string &errMsg);
+
         /* Creates a set with a given type for an existing database */
         bool createSet(const std::string& databaseName, const std::string& setName, const std::string& typeName,
                        std::string& errMsg, size_t pageSize=DEFAULT_PAGE_SIZE);
-
-        /* Creates a temporary set with a given type for an existing database (only goes through storage) */
-        bool createTempSet(const std::string& databaseName, const std::string& setName, const std::string& typeName,
-                           std::string& errMsg, size_t pageSize=DEFAULT_PAGE_SIZE);
 
         /* Creates a set with a given type (using a template) for an existing database */
         template <class DataType>
         bool createSet (const std :: string& databaseName, const std :: string& setName, std :: string &errMsg,
                         size_t pageSize = DEFAULT_PAGE_SIZE);
+
+        /* Creates a temporary set with a given type for an existing database (only goes through storage) */
+        bool createTempSet(const std::string& databaseName, const std::string& setName, const std::string& typeName,
+                           std::string& errMsg, size_t pageSize=DEFAULT_PAGE_SIZE);
 
         /* Flushes data currently in memory into disk. */
         bool flushData ( std :: string &errMsg );
