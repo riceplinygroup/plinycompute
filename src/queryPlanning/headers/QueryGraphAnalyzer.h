d8adcadddd216b4fd286701c7c0b795a773628a4
jia-bigdata <jacquelinezou@gmail.com>
2017-10-20 15:45:57 +0000

    Cleanup for folder queryPlanning
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3707 522aa1da-077c-40de-a48b-1771163b7009

--- src/queryPlanning/headers/QueryGraphAnalyzer.h
+++ src/queryPlanning/headers/QueryGraphAnalyzer.h
@@ -18,16 +18,13 @@
 #ifndef QUERY_GRAPH_ANALYZER_HEADER
 #define QUERY_GRAPH_ANALYZER_HEADER
 
-// by Jia, Mar 2017
-
-#include "Handle.h"
 #include "Computation.h"
+#include "Handle.h"
 #include "InputTupleSetSpecifier.h"
 #include "PDBVector.h"
 #include <vector>
 
 namespace pdb {
-
 
 /*
  * This class encapsulates the analyzer to user query graph
@@ -36,44 +33,42 @@ namespace pdb {
 class QueryGraphAnalyzer {
 
 public:
-    // constructor
-    QueryGraphAnalyzer(std::vector<Handle<Computation>>& queryGraph);
+  // constructor
+  QueryGraphAnalyzer(std::vector<Handle<Computation>> &queryGraph);
 
-    // constructor
-    QueryGraphAnalyzer(Handle<Vector<Handle<Computation>>> queryGraph);
+  // constructor
+  QueryGraphAnalyzer(Handle<Vector<Handle<Computation>>> queryGraph);
 
-    // to convert user query to a tcap string
-    std::string parseTCAPString();
+  // to convert user query to a tcap string
+  std::string parseTCAPString();
 
-    // to traverse the sub-tree and put each traversed computation to a vector
-    void parseComputations(std::vector<Handle<Computation>>& computations,
-                           Handle<Computation> sink);
+  // to traverse the sub-tree and put each traversed computation to a vector
+  void parseComputations(std::vector<Handle<Computation>> &computations,
+                         Handle<Computation> sink);
 
-    // to convert user query to a pdb::Vector of computations
-    // this method will invoke makeObject, but will not allocate allocation blocks
-    // you must ensure current allocation block has sufficient memory before invoking this method
-    void parseComputations(std::vector<Handle<Computation>>& computations);
+  // to convert user query to a pdb::Vector of computations
+  // this method will invoke makeObject, but will not allocate allocation blocks
+  // you must ensure current allocation block has sufficient memory before
+  // invoking this method
+  void parseComputations(std::vector<Handle<Computation>> &computations);
 
-    // to traverse from a graph sink recursively
-    void traverse(std::vector<std::string>& tcapStrings,
-                  Handle<Computation> sink,
-                  std::vector<InputTupleSetSpecifier> inputTupleSets,
-                  int& computationLabel,
-                  std::string& outputTupleSetName,
-                  std::vector<std::string>& outputColumnNames,
-                  std::string& addedOutputColumnName);
+  // to traverse from a graph sink recursively
+  void traverse(std::vector<std::string> &tcapStrings, Handle<Computation> sink,
+                std::vector<InputTupleSetSpecifier> inputTupleSets,
+                int &computationLabel, std::string &outputTupleSetName,
+                std::vector<std::string> &outputColumnNames,
+                std::string &addedOutputColumnName);
 
-    // to clear traversal marks on the subtree rooted at sink
-    void clearGraphMarks(Handle<Computation> sink);
+  // to clear traversal marks on the subtree rooted at sink
+  void clearGraphMarks(Handle<Computation> sink);
 
-    // to clear all traversal marks
-    void clearGraphMarks();
+  // to clear all traversal marks
+  void clearGraphMarks();
 
 private:
-    // user query graph
-    std::vector<Handle<Computation>> queryGraph;
+  // user query graph
+  std::vector<Handle<Computation>> queryGraph;
 };
 }
-
 
 #endif
