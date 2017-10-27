7786f694caf2858872da58883c9b7ccaad7d9424
Carlos Monroy <cm13@rice.edu>
2017-10-27 17:08:39 +0000

    fix print method
    git-svn-id: https://svn.rice.edu/r/software_0/ObjectQueryModel@3809 522aa1da-077c-40de-a48b-1771163b7009

--- src/serverFunctionalities/source/CatalogClient.cc
+++ src/serverFunctionalities/source/CatalogClient.cc
@@ -642,25 +642,21 @@ bool CatalogClient::printCatalogMetadata(std::string &categoryToPrint,
     pdb::Handle<pdb::CatalogPrintMetadata> itemToPrint =
             pdb::makeObject<CatalogPrintMetadata>("","",categoryToPrint);
 
-    return simpleRequest<CatalogPrintMetadata, SimpleRequestResult, bool>(
+    return simpleRequest<pdb::CatalogPrintMetadata, CatalogPrintMetadata, bool>(
         myLogger,
         port,
         address,
         false,
         1024,
-        [&](Handle<SimpleRequestResult> result) {
+        [&](Handle<CatalogPrintMetadata> result) {
             if (result != nullptr) {
-                if (!result->getRes().first) {
-                    errMsg = "Error printing catalog metadata: " + result->getRes().second;
-                    myLogger->error("Error printing catalog metadata: " + result->getRes().second);
-                    return false;
-                }
+                cout << result->getMetadataToPrint() << endl;
                 return true;
             }
             errMsg = "Error printing catalog metadata.";
             return false;
         },
-        itemToPrint);
+        itemToSearch);
 }
 
 
