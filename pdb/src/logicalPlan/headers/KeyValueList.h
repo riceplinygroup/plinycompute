b445cbf46834b1fdc6b42d54a0290a1f5bf78308
Sourav Sikdar <ss107@rice.edu>
2018-01-29 19:56:05 -0600

    Adding support for a list of key-value pairs

---
+++ pdb/src/logicalPlan/headers/KeyValueList.h
@@ -0,0 +1,59 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef KEY_VALUE_LIST_H
+#define KEY_VALUE_LIST_H
+
+#include <iostream>
+#include <memory>
+#include <stdlib.h>
+#include <string>
+#include <utility>
+#include <vector>
+#include <map>
+#include <unordered_map>
+
+// this is a list of key value pairs... used to build up schema specifications
+struct KeyValueList {
+
+private:
+
+	std::unordered_map <std :: string, std :: string> keyValuePairs;
+
+public:
+
+    	~KeyValueList () {}
+    	KeyValueList () {}
+
+
+	void appendkeyValuePair (char *keyName, char *valueName) {
+		keyValuePairs[std :: string (keyName)] = std :: string (valueName);
+	}
+
+
+
+	std :: unordered_map<std :: string, std :: string> &getKeyValuePairs () {
+		return keyValuePairs;
+	}
+	
+	//friend struct TupleSpec;
+
+};
+
+#endif
+
