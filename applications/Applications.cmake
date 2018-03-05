e5b0c7f716c759d1da5a334388e7c02af74900a4
Carlos Monroy <carlos.monroy@rice.edu>
2018-03-05 15:40:40 -0600

    fixes in make files

--- applications/Applications.cmake
+++ applications/Applications.cmake
@@ -1,3 +1,20 @@
+# adds LA test to CMAKE
+function(add_pdb_LA_application test-name)
+    # get the directory
+    get_filename_component(test-path ${CMAKE_CURRENT_LIST_FILE} DIRECTORY)
+
+    # create the target
+    add_executable(${test-name} "${test-path}/tests/${test-name}.cc"
+            $<TARGET_OBJECTS:logical-plan-parser>
+            $<TARGET_OBJECTS:pdb-client>
+            $<TARGET_OBJECTS:linear-algebra-parser>)
+
+    # link it to the required libraries
+    target_link_libraries(${test-name} pdb-tests-common)
+    target_link_libraries(${test-name} ${GSL_LIBRARIES})
+endfunction(add_pdb_LA_application)
+
+
 # adds one pdb integration test to CMAKE
 function(add_pdb_application test-name)
     # get the directory
