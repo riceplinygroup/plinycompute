f6c68d77109e0586e0c63bca5f989baf2f16507b
Dimitrije Jankov <dimitrijejankov@gmail.com>
2018-03-20 16:10:30 -0500

    Fixed a compilation error

--- tests/unit/TestUserGraphAnalysis.cc
+++ tests/unit/TestUserGraphAnalysis.cc
@@ -38,74 +38,100 @@
 #include <IntAggregation.h>
 #include <IntSimpleJoin.h>
 #include <StringSelectionOfStringIntPair.h>
+#include <SimplePhysicalOptimizer/SimplePhysicalNodeFactory.h>
 
 using namespace pdb;
-int main(int argc, char* argv[]) {
-    const UseTemporaryAllocationBlock myBlock{36 * 1024 * 1024};
+int main(int argc, char *argv[]) {
+  const UseTemporaryAllocationBlock myBlock{36 * 1024 * 1024};
 
-    // create all of the computation objects
-    Handle<Computation> myScanSet1 = makeObject<ScanUserSet<int>>("test78_db", "test78_set1");
-    Handle<Computation> myScanSet2 = makeObject<ScanUserSet<StringIntPair>>("test78_db", "test78_set2");
-    Handle<Computation> mySelection = makeObject<StringSelectionOfStringIntPair>();
-    mySelection->setInput(myScanSet2);
-    Handle<Computation> myJoin = makeObject<IntSimpleJoin>();
-    myJoin->setInput(0, myScanSet1);
-    myJoin->setInput(1, myScanSet2);
-    myJoin->setInput(2, mySelection);
-    Handle<Computation> myAggregation = makeObject<IntAggregation>();
-    myAggregation->setInput(myJoin);
-    Handle<Computation> myWriter = makeObject<WriteUserSet<SumResult>>("test78_db", "output_set1");
-    myWriter->setInput(myAggregation);
+  // create all of the computation objects
+  Handle<Computation> myScanSet1 = makeObject<ScanUserSet<int>>("test78_db", "test78_set1");
+  Handle<Computation> myScanSet2 = makeObject<ScanUserSet<StringIntPair>>("test78_db", "test78_set2");
+  Handle<Computation> mySelection = makeObject<StringSelectionOfStringIntPair>();
+  mySelection->setInput(myScanSet2);
+  Handle<Computation> myJoin = makeObject<IntSimpleJoin>();
+  myJoin->setInput(0, myScanSet1);
+  myJoin->setInput(1, myScanSet2);
+  myJoin->setInput(2, mySelection);
+  Handle<Computation> myAggregation = makeObject<IntAggregation>();
+  myAggregation->setInput(myJoin);
+  Handle<Computation> myWriter = makeObject<WriteUserSet<SumResult>>("test78_db", "output_set1");
+  myWriter->setInput(myAggregation);
 
-    std::vector<Handle<Computation>> queryGraph;
-    queryGraph.push_back(myWriter);
-    QueryGraphAnalyzer queryAnalyzer(queryGraph);
-    std::string tcapString = queryAnalyzer.parseTCAPString();
-    std::cout << "TCAP OUTPUT:" << std::endl;
-    std::cout << tcapString << std::endl;
-    std::vector<Handle<Computation>> computations;
-    std::cout << "PARSE COMPUTATIONS..." << std::endl;
-    queryAnalyzer.parseComputations(computations);
-    Handle<Vector<Handle<Computation>>> computationsToSend = makeObject<Vector<Handle<Computation>>>();
+  std::vector<Handle<Computation>> queryGraph;
+  queryGraph.push_back(myWriter);
+  QueryGraphAnalyzer queryAnalyzer(queryGraph);
+  std::string tcapString = queryAnalyzer.parseTCAPString();
+  std::cout << "TCAP OUTPUT:" << std::endl;
+  std::cout << tcapString << std::endl;
+  std::vector<Handle<Computation>> computations;
+  std::cout << "PARSE COMPUTATIONS..." << std::endl;
+  queryAnalyzer.parseComputations(computations);
+  Handle<Vector<Handle<Computation>>> computationsToSend = makeObject<Vector<Handle<Computation>>>();
 
-    for (const auto &computation : computations) {
-        computationsToSend->push_back(computation);
+  for (const auto &computation : computations) {
+    computationsToSend->push_back(computation);
+  }
+
+  AbstractPhysicalNodeFactoryPtr analyzerNodeFactory;
+
+  std::vector<AtomicComputationPtr> sourcesComputations;
+  PDBLoggerPtr logger = make_shared<PDBLogger>("testSelectionAnalysis.log");
+  ConfigurationPtr conf = make_shared<Configuration>();
+  std::string jobId = "TestSelectionJob";
+
+  try {
+    // parse the plan and initialize the values we need
+    Handle<ComputePlan> computePlan = makeObject<ComputePlan>(String(tcapString), *computationsToSend);
+    LogicalPlanPtr logicalPlan = computePlan->getPlan();
+    AtomicComputationList computationGraph = logicalPlan->getComputations();
+    sourcesComputations = computationGraph.getAllScanSets();
+
+    // this is the tcap analyzer node factory we want to use create the graph for the physical analysis
+    analyzerNodeFactory = make_shared<SimplePhysicalNodeFactory>(jobId, computePlan, conf);
+  }
+  catch (pdb::NotEnoughSpace &n) {
+
+    std::cout << "Test failed" << std::endl;
+    return -1;
+  }
+
+  // generate the analysis graph (it is a list of source nodes for that graph)
+  auto graph = analyzerNodeFactory->generateAnalyzerGraph(sourcesComputations);
+
+  // initialize the physicalAnalyzer - used to generate the pipelines and pipeline stages we need to execute
+  PhysicalOptimizer physicalOptimizer(graph, logger);
+
+  std::vector<Handle<AbstractJobStage>> queryPlan;
+  std::vector<Handle<SetIdentifier>> interGlobalSets;
+  std::cout << "PARSE TCAP STRING..." << std::endl;
+
+  int jobStageId = 0;
+  StatisticsPtr statsForOptimization = nullptr;
+  while (physicalOptimizer.hasSources()) {
+
+    // get the next sequence of stages returns false if it selects the wrong source, and needs to retry it
+    bool success = physicalOptimizer.getNextStagesOptimized(queryPlan,
+                                                            interGlobalSets,
+                                                            statsForOptimization,
+                                                            jobStageId);
+
+    if (success) {
+      std::cout << "PRINT PHYSICAL PLAN..." << std::endl;
+      for (int i = 0; i < queryPlan.size(); i++) {
+        std::cout << "to print the " << i << "-th plan" << std::endl;
+        queryPlan[i]->print();
+      }
     }
+  }
 
-    PDBLoggerPtr logger = make_shared<PDBLogger>("testSelectionAnalysis.log");
-    ConfigurationPtr conf = make_shared<Configuration>();
-    std::string jobId = "TestSelectionJob";
+  int code = system("scripts/cleanupSoFiles.sh");
+  if (code < 0) {
+    std::cout << "Can't cleanup so files" << std::endl;
+  }
 
-    PhysicalOptimizer physicalOptimizer(jobId, logger, conf, tcapString, computationsToSend);
-    std::vector<Handle<AbstractJobStage>> queryPlan;
-    std::vector<Handle<SetIdentifier>> interGlobalSets;
-    std::cout << "PARSE TCAP STRING..." << std::endl;
-    
-    int jobStageId = 0;
-    StatisticsPtr statsForOptimization = nullptr;
-    while (physicalOptimizer.hasSources()) {
+  getAllocator().cleanInactiveBlocks(36 * 1024 * 1024);
 
-        // get the next sequence of stages returns false if it selects the wrong source, and needs to retry it
-        bool success = physicalOptimizer.getNextStagesOptimized(queryPlan,
-                                                           interGlobalSets,
-                                                           statsForOptimization,
-                                                           jobStageId);
-
-        if(success) {
-            std::cout << "PRINT PHYSICAL PLAN..." << std::endl;
-            for (int i = 0; i < queryPlan.size(); i++) {
-                std::cout << "to print the " << i << "-th plan" << std::endl;
-                queryPlan[i]->print();
-            }
-        }
-    }
-
-    int code = system("scripts/cleanupSoFiles.sh");
-    if (code < 0) {
-        std::cout << "Can't cleanup so files" << std::endl;
-    }
-
-    getAllocator().cleanInactiveBlocks(36 * 1024 * 1024);
 }
 
 #endif
