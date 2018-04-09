33f909c5b9acb0119fb6cf2a7acdea3668b95d1d
jiazou-bigdata <jacquelinezou@gmail.com>
2018-04-09 19:30:45 +0000

    add computation object for partition

---
+++ pdb/src/computations/headers/PartitionComp.h
@@ -0,0 +1,41 @@
+/*****************************************************************************
+ *                                                                           *
+ *  Copyright 2018 Rice University                                           *
+ *                                                                           *
+ *  Licensed under the Apache License, Version 2.0 (the "License");          *
+ *  you may not use this file except in compliance with the License.         *
+ *  You may obtain a copy of the License at                                  *
+ *                                                                           *
+ *      http://www.apache.org/licenses/LICENSE-2.0                           *
+ *                                                                           *
+ *  Unless required by applicable law or agreed to in writing, software      *
+ *  distributed under the License is distributed on an "AS IS" BASIS,        *
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. *
+ *  See the License for the specific language governing permissions and      *
+ *  limitations under the License.                                           *
+ *                                                                           *
+ *****************************************************************************/
+
+#ifndef PARTITION_COMP
+#define PARTITION_COMP
+
+#include "PartitionCompBase.h"
+
+namespace pdb {
+
+template<class OutputClass, class InputClass>
+class PartitionComp : public PartitionCompBase<OutputClass, InputClass> {
+public:
+
+  /**
+   * the computation returned by this method is called to perfom a transformation on the input
+   * item before it is inserted into the output set
+   * @param checkMe: the input element
+   * @return: the output lambda tree used to apply to the input element
+   */
+  virtual Lambda<Handle<OutputClass>> getProjection(Handle<InputClass> checkMe) = 0;
+
+};
+}
+
+#endif
