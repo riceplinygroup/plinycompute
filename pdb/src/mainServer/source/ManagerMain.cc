fb85358e0ea859c79b9e3eac484d2a2cd5b7554a
Dimitrije Jankov <dimitrijejankov@gmail.com>
2018-09-11 17:35:28 -0500

    Small fix in the manager

--- pdb/src/mainServer/source/ManagerMain.cc
+++ pdb/src/mainServer/source/ManagerMain.cc
@@ -115,7 +115,7 @@ int main(int argc, char* argv[]) {
        portValue = (*allNodes)[i]->getPort();
 
        // register the worker
-       if(!frontEnd.getFunctionality<pdb::CatalogServer>().getCatalog()->registerNode(std::make_shared<pdb::PDBCatalogNode>(hostName + ":" + std::to_string(port),
+       if(!frontEnd.getFunctionality<pdb::CatalogServer>().getCatalog()->registerNode(std::make_shared<pdb::PDBCatalogNode>(hostName + ":" + std::to_string(portValue),
                                                                                                                             hostName,
                                                                                                                             portValue,
                                                                                                                             "worker"), errMsg)) {
